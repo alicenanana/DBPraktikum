e8131152220b8a9be0e4512bb8a5f85d0598f484
JaceBelleren <robin.gellner.25@gmail.com>
2025-07-02 23:04:14 +0200

    Update UserInputs.java

--- src/main/java/UserInputs.java
+++ src/main/java/UserInputs.java
@@ -14,7 +14,7 @@ public class UserInputs {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String user = "postgres";
-        String password = "Robin2504!";
+        String password = "postgres";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             String asin = "B00005N5PF";
